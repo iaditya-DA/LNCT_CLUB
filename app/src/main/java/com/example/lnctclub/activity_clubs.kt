702190d7b18eabdff457edc82584b822ff096cd3
ADITYA KUMAR JHA <adityakumarrjhaa@gmail.com>
2025-08-11 02:23:53 +0530

    changes done

---
+++ app/src/main/java/com/example/lnctclub/activity_clubs.kt
@@ -0,0 +1,11 @@
+package com.example.lnctclub
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ClubsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clubs) // XML file: activity_clubs.xml
+    }
+}
---
+++ app/src/main/java/com/example/lnctclub/activity_profile.kt
@@ -0,0 +1,11 @@
+package com.example.lnctclub
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile) // XML file: activity_profile.xml
+    }
+}
